80647a03a1eacfd50aa1f90eb192131a21d9ca1d
Huynh Minh Tu <tuhm@Huynhs-MBP.net.fpt>
2018-03-28 20:32:55 +0700

    tuhm - update code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,10 +29,10 @@ class Header extends React.Component {
               height="38"
               alt="React"
             />
-            <span className={s.brandTxt}>Your Company</span>
+            <span className={s.brandTxt}>FU Team</span>
           </Link>
           <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
+            <h1 className={s.bannerTitle}>FUCMS</h1>
             <p className={s.bannerDesc}>Complex web apps made easy</p>
           </div>
         </div>
